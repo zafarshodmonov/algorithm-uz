74fc77c26034e06d6e5cfe2c4564ed38ebb346c3
zafarshodmonov <zafarshodmonov1996@gmail.com>
2023-10-13 22:03:10 +0500

    z

--- zarequests.py
+++ zarequests.py
@@ -1,12 +1,8 @@
 import requests
 
-def main():
-    # url = 'https://api.github.com/events'
+def main() -> None:
     url = "https://randomuser.me/api/"
     response = requests.get(url)
-    print(response)
-
-    pass
-
+    
 if __name__ == "__main__":
     main()
